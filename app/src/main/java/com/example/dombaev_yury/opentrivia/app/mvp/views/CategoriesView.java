57c6998d62c774dfa868a5062118011886745156
Yury Dombaev <yury.dombaev@sebbia.com>
2017-09-04 14:59:37 +0300

    - Исправление повторного возвращения на фрагмент

--- app/src/main/java/com/example/dombaev_yury/opentrivia/app/mvp/views/CategoriesView.java
+++ app/src/main/java/com/example/dombaev_yury/opentrivia/app/mvp/views/CategoriesView.java
@@ -2,20 +2,22 @@ package com.example.dombaev_yury.opentrivia.app.mvp.views;
 
 import com.arellomobile.mvp.MvpView;
 import com.arellomobile.mvp.viewstate.strategy.AddToEndSingleStrategy;
+import com.arellomobile.mvp.viewstate.strategy.OneExecutionStateStrategy;
 import com.arellomobile.mvp.viewstate.strategy.StateStrategyType;
 import com.example.dombaev_yury.opentrivia.app.model.Category;
 
 import java.util.List;
 
-@StateStrategyType(AddToEndSingleStrategy.class)
 public interface CategoriesView extends MvpView {
 
+    @StateStrategyType(AddToEndSingleStrategy.class)
     void showCategories(List<Category> categories);
 
     void showCategoriesLoadingProgress();
 
     void hideCategoriesLoadingProgress();
 
+    @StateStrategyType(OneExecutionStateStrategy.class)
     void openCategoryScreen(Category category);
 
     void showNoNetworkView();
